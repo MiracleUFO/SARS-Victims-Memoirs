4dd34d901c7dcc83cf28416af1466d2ae6810ada
MircleUFO <miracleufodiama@gmail.com>
2020-10-24 23:47:24 +0100

    Added responsivity to Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import Header from '../components/Header';
-import Home from '../components/Home';
-import About from '../components/About';
-import Memoir from '../components/Memoir';
-import Footer from '../components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Memoir from './components/Memoir';
+import Footer from './components/Footer';
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import logo from '../images/header/logo.png';
+import searchIcon from '../images/header/searchIcon.png';
+import {NavLink} from 'react-router-dom';
 
-function Header() {
+function Header(e) {
+
+  let form = React.createRef();
+  let input = React.createRef();
+  let icon = React.createRef();
+  
+
+  function revealBar(e) {
+    e.preventDefault();
+    input.current.setAttribute('class', 'revealField searchField');
+    form.current.setAttribute('class', 'revealForm flexBox4');
+    icon.current.parentElement.style.display = 'none';
+}
+
+  function closeBar(e) {
+    e.preventDefault();
+    form.current.setAttribute('class', 'flexBox4');
+    input.current.setAttribute('class', 'searchField');
+}
+
   return (
-    <header>
-    	<nav className='flexBox flexBox2'>
-    		<NavLink to='/'><img src={logo} alt='' /></NavLink>
-    		<NavLink to='/Memoir'>Memoir</NavLink>
-			<NavLink to='/About'>About</NavLink>
-			<form>
-				<input type='search' name='search' id='searchField'/>
-			</form>
-			<NavLink to='#'><button>Login</button></NavLink>
+    <header className='flexBox2 flexBox3'>
+      <NavLink to='/'><img src={logo} alt='' id='logo'/></NavLink>
+    	<nav className='flexBox2 flexBox3'>
+    	 <NavLink to='/Memoir'>Memoir</NavLink>
+			 <NavLink to='/About'>About</NavLink>
+
+			 <form  ref={form} >
+				  <input type='search' name='search' className='searchField' ref={input} placeholder='Search here...' onBlur={closeBar}/>
+          <button id='searchButton' onClick={revealBar}><img src={searchIcon} alt='' id='icon'  ref={icon}/></button>
+			 </form> 
     	</nav>
+
+      <NavLink to='#'><button>Login</button></NavLink>
     </header>
   );
 }
