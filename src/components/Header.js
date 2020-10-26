16777d23a3517910cfd2a2137b8c599c36f23c14
MircleUFO <miracleufodiama@gmail.com>
2020-10-26 01:24:46 +0100

    Home page done

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,31 +8,40 @@ function Header(e) {
   let form = React.createRef();
   let input = React.createRef();
   let icon = React.createRef();
+  let navTextDiv = React.createRef();
+  let searchButton = React.createRef();
   
 
   function revealBar(e) {
     e.preventDefault();
     input.current.setAttribute('class', 'revealField searchField');
-    form.current.setAttribute('class', 'revealForm flexBox4');
-    icon.current.parentElement.style.display = 'none';
+    input.current.focus();
+    form.current.setAttribute('class', 'revealForm flexBox5');
+    searchButton.current.setAttribute('class', 'searchButtonOnReveal');
+    navTextDiv.current.style.display = 'none';
 }
 
   function closeBar(e) {
     e.preventDefault();
     form.current.setAttribute('class', 'flexBox4');
     input.current.setAttribute('class', 'searchField');
+    navTextDiv.current.style.display = 'flex';
+    searchButton.current.setAttribute('class', '');
 }
 
   return (
     <header className='flexBox2 flexBox3'>
       <NavLink to='/'><img src={logo} alt='' id='logo'/></NavLink>
     	<nav className='flexBox2 flexBox3'>
+
+      <div className='flexBox3' id='navTextDiv' ref={navTextDiv}>
     	 <NavLink to='/Memoir'>Memoir</NavLink>
 			 <NavLink to='/About'>About</NavLink>
+       </div>
 
 			 <form  ref={form} >
 				  <input type='search' name='search' className='searchField' ref={input} placeholder='Search here...' onBlur={closeBar}/>
-          <button id='searchButton' onClick={revealBar}><img src={searchIcon} alt='' id='icon'  ref={icon}/></button>
+          <button id='searchButton' onClick={revealBar} ref={searchButton}><img src={searchIcon} alt='' id='icon'  ref={icon}/></button>
 			 </form> 
     	</nav>
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,136 @@
 import React from 'react';
+import heroPic from '../images/home/heroPic.png';
+import moreIcon from '../images/home/moreIcon.png';
+import victim01 from '../images/home/victimForGrid.png';
+
 
 function Home() {
   return (
-    <main>
-    HOME
+    <main id='home'>
+    	<section id='heroSect'>
+    		<img src={heroPic} alt='' id='heroPic'/>
+    		<img src={moreIcon} alt='' id='moreIcon'/>
+    		<div id='heroBubble' className='flexBox3 flexBox4'><h1>" Delayed <br/> justice <br/> is injustice"</h1></div>
+    	</section>
+
+    	<section>
+    		<h1 className='homeHeaders'>All these were lost through police brutality.</h1>
+
+    		<article className='victimHomeGrid'>
+    			<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>
+
+				<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>    			
+
+    			<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>
+
+    			<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>
+
+    			<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>
+
+    			<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>
+    		</article>
+    	</section>
+
+
+    	<section>
+    		<h1 className='homeHeaders'>Why shoot them? Why inhuman to them?</h1>
+
+    		<article className='victimHomeGrid'>
+    			<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>
+
+				<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>    			
+
+    			<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>
+
+    			<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>
+
+    			<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>
+    			
+    			<div className='flexColumn'>
+    				<img src={victim01} alt='Chimela Obi'/> 
+    				<div><h1>Chimela Obi</h1><p>12/07/1994  - 13/01/2019</p> 
+    				<p>Was exthorted and shot by a police on her way to lectures, she’s the only child of her...</p></div>
+    				<p className='flexBox5'>Read more</p>
+    			</div>
+    		</article>
+    	</section>
+
+    	<article className='flexBox2' id='statsHome'>
+    		<div className='flexColumn'>
+    			<h1 className='text60'>1,223</h1>
+    			<p className='greyText'>Cases recorded this year.</p>
+    		</div>
+
+    		<div className='flexColumn'>
+    			<h1 className='text60'>906</h1>
+    			<p className='greyText'>Cases recorded last year</p>
+    		</div>
+
+    		<div className='flexColumn'>
+    			<h1 className='text60'>1,200</h1>
+    			<p className='greyText'>Youths reportedly recorded</p>
+    		</div>
+
+    		<div className='flexColumn'>
+    			<h1 className='text60'>2,129</h1>
+    			<p className='greyText'>Total cases</p>
+    		</div>
+    	</article>
     </main>
   );
 }
